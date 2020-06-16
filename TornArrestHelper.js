f19d39ca0f6e616df7c3573f0ee7a3967e5e9b02
BuildTools <unconfigured@null.spigotmc.org>
2020-06-16 01:15:31 -0400

    Split stats page functionality into new script

--- TornArrestHelper.js
+++ TornArrestHelper.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name         Torn Arrest Helper
-// @version      1.0
+// @version      1.2
 // @description  Helps find likely arrest targets from the hospital page
 // @author       SixPraxis - [860380]
 // @match        https://www.torn.com/hospitalview.php*
-// @match        https://www.torn.com/personalstats.php*
 // ==/UserScript==
 
 (function() {
@@ -14,9 +13,6 @@
     filterBtn.innerHTML = "Arrest Helper Filter";
     filterBtn.setAttribute("class",  "city t-clear h c-pointer  m-icon line-h24 right last");
     filterBtn.onclick = filterUsers;
-
-    var observer = new MutationObserver(checkForTable);
-    const config = { attributes: false, childList: true, subtree: true};
 
     function addButton(){
     	var linksTopDiv = document.getElementById("top-page-links-list");
@@ -53,33 +49,7 @@
         })
     }
 
-    function calcDiff(table){
-    	let value1 = parseInt(table.getElementsByTagName("tr")[0].getElementsByTagName("td")[1].innerHTML.replace(/[\.,]/, ""));
-    	let value2 = parseInt(table.getElementsByTagName("tr")[15].getElementsByTagName("td")[1].innerHTML.replace(/[\.,]/, ""));
-    	let value3 = parseInt(table.getElementsByTagName("tr")[30].getElementsByTagName("td")[1].innerHTML.replace(/[\.,]/, ""));
-    	let titleDiv = document.getElementsByClassName("title___hNtZa")[1];
-    	let timeFrame = document.getElementsByClassName("toggler")[0].innerHTML;
-    	titleDiv.innerHTML = titleDiv.innerHTML + " | " + (value1-value3) + " in the last " + timeFrame + " | " + (value1-value2) + " in half that time";
-    }
-
-    function observeDiv(){
-    	let reactDiv = document.getElementById("react-root");
-    	observer.observe(reactDiv, config);
-    }
-
-    function checkForTable(){
-    	let table = document.getElementsByTagName("tbody");
-    	if (table.length > 0){
-    		observer.disconnect();
-    		calcDiff(table[0]);
-    	}
-    }
-
     if (document.URL.includes("hospitalview")){
     	window.onload = addButton;
-    }
-
-    if (document.URL.includes("stats=jailed")){
-    	window.onload = observeDiv;
     }
 })();
